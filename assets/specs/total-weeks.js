597a3f131143de5126ef4cd403c804a1e2305eef
Sergio Crisostomo <sergiosbox@gmail.com>
2015-12-15 00:33:47 +0100

    add test data for week nr of january first

--- assets/specs/total-weeks.js
+++ assets/specs/total-weeks.js
@@ -1,18 +1,34 @@
 /*
-
 == Total weeks in years of 1970 to 2050, from PHP: ==
-
 	$arr = [];
 	function getIsoWeeksInYear($year) {
-		$date = new DateTime;
-		$date->setISODate($year, 53);
+		$date = new DateTime($year.'-12-31');
 		return ($date->format("W") === "53" ? 53 : 52);
 	}
 	for ($i = 1970; $i < 2050; $i++){
 		array_push($arr, getIsoWeeksInYear($i));
 	}
 	echo json_encode($arr);
-
 */
 
-module.exports = [53,52,52,52,52,52,53,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,53,52,52,52,52,52,53,52,52,52,52,53,52];
+var totalWeeks = [53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52, 52, 52, 52, 52, 53, 52, 52, 52, 52, 53, 52];
+
+/*
+== Number of week in year for 1st of January ==
+	$arr = [];
+	function getWeek($year) {
+		$date = new DateTime($year.'-01-01');
+		return $date->format("W");
+	}
+	for ($i = 1971; $i < 2051; $i++){
+		array_push($arr, getWeek($i));
+	}
+	echo json_encode($arr);
+*/
+
+var januaryWeekStart = [53, 52, 1, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52, 52, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52, 52, 1, 1, 1, 53, 52, 1, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52, 52, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52, 52, 1, 1, 1, 53, 52, 1, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52, 52, 1, 1, 1, 53, 52, 1, 1, 1, 53, 52];
+
+module.exports = {
+	totalWeeks: totalWeeks,
+	januaryWeekStart: januaryWeekStart
+}
