64de2f9ed905cb44996c2602a0bdb662a0b88799
Sergio Crisostomo <sergiosbox@gmail.com>
2017-03-31 14:17:06 +0200

    export labels to calendar object

--- src/addLabels.js
+++ src/addLabels.js
@@ -2,15 +2,21 @@
 
 var labels = require('./labels');
 
+function isIterable(variable){
+	if (variable == null) return false;
+	if (variable.constructor === Array) return true;
+	if (variable.constructor === Object) return true;
+	return false;
+}
+
 function merge(_new, _old){
     for (var prop in _new){
         if (!_old[prop]) _old[prop] = _new[prop];
-        else merge(_new[prop], _old[prop]);
+        else if (isIterable(_new[prop])) merge(_new[prop], _old[prop]);
     }
 }
 
 function addLabels(dayObject, lang){
-
 	var cssClass = [labels.classes[dayObject.type]];
 
 	if (dayObject.class) dayObject.class = (typeof dayObject.class == 'string' ? [dayObject.class] : dayObject.class).concat(cssClass);
@@ -20,10 +26,15 @@ function addLabels(dayObject, lang){
 		if (dayObject.index == 0 && labels.weekPlaceholder) dayObject.desc = labels.weekPlaceholder;
 		else if (dayObject.index < 8) dayObject.desc = labels.columnNames[lang][dayObject.index];
 		else if (dayObject.index % 8 == 0) dayObject.desc = dayObject.week;
-	} 
+	}
 
 	if (dayObject.date) dayObject.monthName = labels.monthNames[lang][dayObject.date.getMonth()];
 	if (!this.monthName) this.monthName = labels.monthNames[lang][this.month];
+	if (!this.labels) this.labels = {
+		monthNames: labels.monthNames[lang],
+		columnNames: labels.columnNames[lang],
+		classes: labels.classes
+	}
 
 	return dayObject;
 }
